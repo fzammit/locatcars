74610b1a73490ba6456a15f93b7682cf7b3b8f87
Fzammit <frederic.zammit@hotmail.fr>
2020-02-10 16:00:22 +0100

    prepa config

--- config/config.php
+++ config/config.php
@@ -1,4 +1,9 @@
 <?php
 
-require_once __DIR__ .'/../vendor/autoload.php';
-require_once __DIR__ .'/routes.php';
+use App\Service\ServiceContainer;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$container = new ServiceContainer;
+
+require_once __DIR__ . '/routes.php';
--- config/routes.php
+++ config/routes.php
@@ -1,11 +1,13 @@
 <?php
 
-use Bramus\Router\Router;
-
-$router = new Router;
+$router = $container->getRouter();
 
 $router->get('/hello', function() {
     echo "Hello world !";
+});
+
+$router->get('/', function () {
+    echo "Bienvenue sur mon site !";
 });
 
 $router->run();
